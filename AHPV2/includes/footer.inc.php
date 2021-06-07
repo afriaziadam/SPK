a51625f23ff867c514038008ff15b72d90772df0
afriaziadam <49552508+afriaziadam@users.noreply.github.com>
2021-06-07 16:06:59 +0700

    Add files via upload

---
+++ AHPV2/includes/footer.inc.php
@@ -0,0 +1,11 @@
+
+	<footer class="text-center">2021 SPK ITENAS</footer><hr>
+	</div>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="assets/js/dataTables.bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap-datepicker.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.toastmessage.js"></script>
+		<script type="text/javascript" src="assets/js/app.js"></script>
+  </body>
+</html>
